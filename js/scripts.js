5626a9700550e8c16402e7219e6503d931ea411c
Amanda Daly and Priya Vasudevan <satipriya99@gmail.com>
2016-03-23 21:00:52 -0700

    It works atlast.

--- js/scripts.js
+++ js/scripts.js
@@ -2,54 +2,59 @@
 function Account (name, amount){
   this.name = name;
   this.amount = [];
-/*  deposit: function(balance){
-    this.balance = this.balance + amount;
-  }
-  withdrawal: function(balance){
-    this.amount = this.blance - amount;
-  }*/
 };
 
 var add = function(a, b){
-  return a + b;
+  return a+b;
 }
 
-var newTotal = function(){
-  Account.amount.reduce(add);
-}
 
 
 //User Interface Logic
+
+//User Name and Initial Deposit Form
 $("form#signup").submit(function(event) {
   event.preventDefault();
 
+  //Event Listeners, get info from user fields
   var userName = $("input#name").val();
   var userInitDeposit = parseInt($("input#initial-deposit").val());
 
-  var newAccount = new Account(userName, userInitDeposit);
-
+  //Create new Account object with the user input and push the initialDeposit to the amount array
+  var newAccount = new Account(userName);
   newAccount.amount.push(userInitDeposit);
 
+  //Manipulate the CSS to now show the Deposit and Withdrawal form and hide the signup form, as well as show the initialDeposit amount under Balance
   $("#signup").hide();
   $("#transaction").show();
   $("#account-balance").append("<li>" + newAccount.amount + "</li>");
   $("#balance").show();
 
+
+    //Deposit and Withdrawal Forms
     $("form#transaction").submit(function(event) {
       event.preventDefault();
 
-      var userDeposit = parseInt($("input#deposit").val());
-      var userWithdrawal = parseInt($("input#withdrawal").val());
+      //Event Listeners, get input from user in deposit and withdrawal fields
 
-      newAccount.amount.push(userDeposit);
-      newAccount.amount.push(userWithdrawal);
+      //Push the withdrawal and deposit amounts into the newAccount object amount value
+      $("#transaction").each(function() {
+        var userDeposit = $(this).find("input#deposit").val();
+        var userWithdrawal = $(this).find("input#withdrawal").val();
+
+        newAccount.amount.push(parseInt(userDeposit));
+        newAccount.amount.push(parseInt(userWithdrawal));
+      });
+
+      //Add all of the values in the amount array and return a single value, using the add function in the business logic section
+
+      console.log(newAccount.amount.reduce(add));
 
       console.log(newAccount.amount);
 
-      newTotal(newAccount.amount);
-
-      $("#account-balance").prepend("<li>" + newAccount.amount + "</li>");
-      $("#balance").show();
+      //Clears the withdrawal and deposit fields to allow for new entries
+      $("input#deposit").val("");
+      $("input#withdrawal").val("");
 
     });
 });
---
+++ randam.js
@@ -0,0 +1,7 @@
+function Account (name, balance){
+this.name = name;
+this.balance = [];
+}
+var deposit = function(){this.balance.push(Math.abs(parseInt(userDeposit)))};
+var withdrawal = function(){this.balance.push(-Math.abs(parseInt(userWithdrawal)))};
+var newBalance = function(){};
